3567d9589bd244c8234f4e22f186c75dca9823ae
Dat <dndat1122@gmail.com>
2024-04-06 13:45:48 +0700

    first commit

---
+++ API/Controller/UsersController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int Id)
+        {
+            return await _context.Users.FindAsync(Id);
+
+        }
+    }
+
+
+
+}
