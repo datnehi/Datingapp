60538f606db2cbe9d80762b3bd5d258c0105b23c
Dat <dndat1122@gmail.com>
2024-05-05 21:13:49 +0700

    10

--- API/Controller/UsersController.cs
+++ API/Controller/UsersController.cs
@@ -1,4 +1,5 @@
 
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -35,6 +36,16 @@ namespace API.Controller
         {
             return await _userRepository.GetMemberAsync(username);
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update user");
         }
     }
 
---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,15 @@
+import { CanDeactivate } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class preventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  canDeactivate(component: MemberEditComponent): boolean {
+    if(component.editForm.dirty){
+      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
+    }
+    return true;
+  }
+
+};
